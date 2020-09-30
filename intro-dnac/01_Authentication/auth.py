a3a252a8143a9cae2cc9b3f3770cbc46f537068c
thaeussl <tom.haeussler@controlware.de>
2020-09-30 01:53:05 -0700

    inital import

---
+++ intro-dnac/01_Authentication/auth.py
@@ -0,0 +1,34 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+import sys
+
+# Get the absolute path for the directory where this file is located "here"
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the absolute path for the project / repository root
+project_root = os.path.abspath(os.path.join(here, "../.."))
+
+# Extend the system path to include the project root and import the env files
+sys.path.insert(0, project_root)
+
+import env_lab
+
+DNAC_URL = env_lab.DNA_CENTER["host"]
+DNAC_USER = env_lab.DNA_CENTER["username"]
+DNAC_PASS = env_lab.DNA_CENTER["password"]
+
+def get_auth_token():
+    """
+    Building out Auth request. Using requests.post to make a call to the Auth Endpoint
+    """
+    url = 'https://{}/dna/system/api/v1/auth/token'.format(DNAC_URL)                      # Endpoint URL
+    hdr = {'content-type' : 'application/json'}                                           # Define request header
+    resp = requests.post(url, auth=HTTPBasicAuth(DNAC_USER, DNAC_PASS), headers=hdr)      # Make the POST Request
+    token = resp.json()['Token']                                                          # Retrieve the Token
+    print("Token Retrieved: {}".format(token))                                            # Print out the Token
+    return token    # Create a return statement to send the token back for later use
+
+
+if __name__ == "__main__":
+    get_auth_token()
